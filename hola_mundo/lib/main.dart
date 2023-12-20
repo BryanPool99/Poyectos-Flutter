49068a1ab6397a3ba7508d1593e987594e441d56
Bryan <bryanpool991@hotmail.com>
2023-12-20 16:38:54 -0500

    proyecto de hola mundo con flutter

---
+++ hola_mundo/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MiApp());
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Mi app",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mi App"),
+      ),
+      body: Text("Hola mundo"),
+    );
+  }
+}
