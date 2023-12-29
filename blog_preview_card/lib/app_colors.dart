012aa7d0e1539ade5a0a0550c0fd6bda25e36e0c
Bryan <bryanpool991@hotmail.com>
2023-12-28 19:22:31 -0500

    archivo dart de colores

---
+++ blog_preview_card/lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryYellow = Color(0xFFFFD700); // HSL: (47, 88%, 63%)
+  static const Color neutralWhite = Color(0xFFFFFFFF); // HSL: (0, 0%, 100%)
+  static const Color neutralGrey = Color(0xFF808080);   // HSL: (0, 0%, 50%)
+  static const Color neutralBlack = Color(0xFF1C1C1C);   // HSL: (0, 0%, 7%)
+}
